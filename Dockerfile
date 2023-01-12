5a7a8a5550511b29ac144bbf1bf8ca5b2cd04b14
SWojciechowski <slawomir.wojciechowski@luxoft.com>
2023-01-12 15:03:54 +0100

    more changes for fun and tests

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM python:3-slim AS builder
 
+COPY requirements.txt /requirements.txt
+
+RUN pip install -r requirements.txt
+
 COPY main.py /main.py
 
 CMD ["python", "/main.py"]
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-import os
-name = 'my_name'
-value = 'my_value'
-with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
-    print(f'{name}={value}', file=fh)
-    print(f'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-    print(fh)
+from github import Github
+
+# First create a Github instance:
+
+# using an access token
+g = Github("access_token")
+
+# Github Enterprise with custom hostname
+g = Github(base_url="https://{hostname}/api/v3", login_or_token="access_token")
+
+# Then play with your Github objects:
+for repo in g.get_user().get_repos():
+    print(repo.name)
+    print(dir(repo))
