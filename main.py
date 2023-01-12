671ad156c21f889064ebc52c790ce4bd73108b3a
SWojciechowski <slawomir.wojciechowski@luxoft.com>
2023-01-12 14:25:38 +0100

    just for more readable output in demo

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ name = 'my_name'
 value = 'my_value'
 with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
     print(f'{name}={value}', file=fh)
+    print(f'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    print(fh)
